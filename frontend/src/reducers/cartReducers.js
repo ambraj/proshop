3b976e5b4114692aafb2ef1693b240feef197a6b
ambuj <ambraj@gmail.com>
2020-11-08 00:49:12 +0530

    bug fixing

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -7,7 +7,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     case CART_ADD_ITEM:
       const item = action.payload;
 
-      const existItem = state.cartItems.find((x) => x.product === itemm.product);
+      const existItem = state.cartItems.find((x) => x.product === item.product);
 
       if (existItem) {
         return {
